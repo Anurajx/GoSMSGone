27757c40a65386ab774fe4be94405c6d33bdd80b
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2026-09-09 20:17:22 +0530

    cap and len for slices

---
+++ src/makingSlice.go
@@ -0,0 +1,11 @@
+package main
+
+func getMessageCosts(messages []string) []float64 {
+	// ?
+	cost := make([]float64, len(messages))
+	for i := 0; i < len(messages); i++ {
+		cost[i] = float64(len(messages[i])) * 0.01
+	}
+	return cost
+
+}
